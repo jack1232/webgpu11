430d9aebd494b8c0e9df8ed27c0daed0857768e1
jack <jackjack5810@yahoo.com>
2021-08-20 14:32:41 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -49,7 +49,7 @@ const Create3DObject = async (isAnimation = true) => {
             entryPoint: "main",
             targets: [
                 {
-                    format: gpu.swapChainFormat as GPUTextureFormat
+                    format: gpu.format as GPUTextureFormat
                 }
             ]
         },
@@ -93,7 +93,7 @@ const Create3DObject = async (isAnimation = true) => {
         }]
     });
 
-    let textureView = gpu.swapChain.getCurrentTexture().createView();
+    let textureView = gpu.context.getCurrentTexture().createView();
     const depthTexture = device.createTexture({
         size: [gpu.canvas.width, gpu.canvas.height, 1],
         format: "depth24plus",
@@ -126,7 +126,7 @@ const Create3DObject = async (isAnimation = true) => {
         CreateTransforms(modelMatrix,[0,0,0], rotation);
         mat4.multiply(mvpMatrix, vpMatrix, modelMatrix);
         device.queue.writeBuffer(uniformBuffer, 0, mvpMatrix as ArrayBuffer);
-        textureView = gpu.swapChain.getCurrentTexture().createView();
+        textureView = gpu.context.getCurrentTexture().createView();
         renderPassDescription.colorAttachments[0].view = textureView;
         const commandEncoder = device.createCommandEncoder();
         const renderPass = commandEncoder.beginRenderPass(renderPassDescription as GPURenderPassDescriptor);
